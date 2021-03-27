e38b0bbe060e9151cd6a02bc436eae926733250c
chen-wangming <2831081986@qq.com>
2021-03-27 16:59:59 +0800

    add: move to dev

--- src/com/kingsware/Application.java
+++ src/com/kingsware/Application.java
@@ -9,6 +9,6 @@ public class Application {
     public static void main(String[] args) {
        // master
 
-        System.out.println("hello world");
+        System.out.println("move to dev");
     }
 }
